e5fc230e8c8b49baf84ffd8770a9a544264485ce
Michael Pollente <pollentem@gmail.com>
2022-03-16 14:39:22 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkMode: 'media',
+  content: [
+    './src/**/*.{html,ts}'
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Roboto']
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: [],
+}
